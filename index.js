6953a6619471c7f42926457e8ea9f3255ed61983
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-03-16 18:25:57 -0300

    added TS

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const myLibrary = []
-
-function Book() {
-    
+var exampleBook = {
+    name: "Design Patterns",
+    author: "Erich Gamma, John Vlissides, Richard Helm, Ralph Johnson",
+    year: 1994
+};
+var myLibrary = [];
+function Book(bookname, author, year) {
+    this.name = name;
 }
-function addBook{
-    
+function addBook() {
 }
---
+++ index.ts
@@ -0,0 +1,14 @@
+const exampleBook = {
+    name: "Design Patterns",
+    author: "Erich Gamma, John Vlissides, Richard Helm, Ralph Johnson",
+    year: 1994 
+}
+const myLibrary = []
+
+function Book(bookname:string, author:string, year:number) {
+    this.name = name;
+
+}
+function addBook{
+
+}
