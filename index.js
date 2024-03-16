36bfbeb4b68f543c218d2c6feacce4bbbee54641
Jonee2 <94491571+Jonee2@users.noreply.github.com>
2024-03-16 20:23:22 -0300

    added factory and getinfo() in object

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-var exampleBook = {
+"use strict";
+const exampleBook = {
     name: "Design Patterns",
-    author: "Erich Gamma, John Vlissides, Richard Helm, Ralph Johnson",
-    year: 1994
+    author: "Erich Gamma",
+    year: 1994,
+    read: false,
+    getInfo() {
+        return `the book name is ${this.name}, 
+    the author is ${this.author}, 
+    the year it came out was ${this.year} 
+    and ${haveRead(this.read)}`;
+    },
 };
-var myLibrary = [];
-function Book(bookname, author, year) {
-    this.name = name;
+const myLibrary = [exampleBook];
+console.log(exampleBook.getInfo());
+function haveRead(read) {
+    if (this.read == false) {
+        return "haven't read it";
+    }
+    else {
+        return "have read it";
+    }
 }
-function addBook() {
+function Book(bookname, author, year, read) {
+    this.name = bookname;
+    this.author = author;
+    this.year = year;
+    this.read = read;
 }
+function addBook() { }
--- index.ts
+++ index.ts
@@ -1,14 +1,32 @@
 const exampleBook = {
-    name: "Design Patterns",
-    author: "Erich Gamma, John Vlissides, Richard Helm, Ralph Johnson",
-    year: 1994 
-}
-const myLibrary = []
+  name: "Design Patterns",
+  author: "Erich Gamma",
+  year: 1994,
+  read: false,
+  getInfo() {
+    return `the book name is ${this.name}, 
+    the author is ${this.author}, 
+    the year it came out was ${this.year} 
+    and ${haveRead(this.read)}`;
+  },
+};
 
-function Book(bookname:string, author:string, year:number) {
-    this.name = name;
+const myLibrary = [exampleBook];
 
-}
-function addBook{
+console.log(exampleBook.getInfo())
 
+function haveRead(this.read) {
+    if (this.read == false) {
+        return "haven't read it"
+    } else {
+        return "have read it"
+    }
 }
+function Book(bookname: string, author: string, year: number, read: boolean) {
+  this.name = bookname;
+  this.author = author;
+  this.year = year;
+  this.read = read;
+}
+
+function addBook() {}
